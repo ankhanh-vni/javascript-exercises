85316a70d296d2e33d00ac204d492bcf7a1c0c34
ankhanh-vni <namla623@gmail.com>
2023-12-29 17:01:01 +0700

    Finished 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reversedString = "";
+    for (let i = word.length - 1; i >= 0; i--) {
+        reversedString += word[i];
+    }
+    return reversedString;
+    // return word.split("").reverse().join("");
 };
 
 // Do not edit below this line
