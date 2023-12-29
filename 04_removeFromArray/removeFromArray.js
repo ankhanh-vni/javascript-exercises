447c30e2b9666d05fbde0a4f89c03b6ce5e8a7ba
ankhanh-vni <namla623@gmail.com>
2023-12-29 17:45:28 +0700

    Finished 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,40 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {;
+    // Straightforward, loop in args and splice the original
+
+    // for (removeItem of args) {
+    //     if (array.includes(removeItem)) {
+    //         array.splice(array.indexOf(removeItem), 1);
+    //     }
+    // }
+
+    // args.forEach(removeItem => {
+    //     if (array.includes(removeItem)) {
+    //         array.splice(array.indexOf(removeItem), 1);
+    //     }
+    // })
+
+    // return array;
+
+
+
+    // Method using forEach
+    let retArr = [];
+    array.forEach(item => {
+        const elementStays = !args.includes(item);
+        if (elementStays) {
+            retArr.push(item);
+        }
+    });
+    return retArr;
+
+
+
+    //Method using filter
+    // return array.filter(element => !args.includes(element));
 
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
