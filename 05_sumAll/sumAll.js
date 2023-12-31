fde1320be57981b9ee10a62388acfb1825167d3f
ankhanh-vni <84317220+ankhanh-vni@users.noreply.github.com>
2023-12-31 18:14:48 +0700

    Refactor sumAll function to use explicit parameters

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,15 @@
-const sumAll = function () {
-    if (arguments.length != 2) return "ERROR";
-    if (typeof arguments[0] !== "number" || typeof arguments[1] !== "number") return "ERROR";
-    if (Math.min(arguments[0], arguments[1]) < 0) return "ERROR";
+const sumAll = function (fromNum, toNum) {
+    // if (arguments.length != 2) return "ERROR";
+    // if (typeof arguments[0] !== "number" || typeof arguments[1] !== "number") return "ERROR";
+    // if (Math.min(arguments[0], arguments[1]) < 0) return "ERROR";
 
-    const fromNum = Math.min(arguments[0], arguments[1]);
-    const toNum = Math.max(arguments[0], arguments[1]);
+    // const fromNum = Math.min(arguments[0], arguments[1]);
+    // const toNum = Math.max(arguments[0], arguments[1]);
+
+    if (!Number.isInteger(fromNum) || !Number.isInteger(toNum)) return "ERROR";
+    if (Math.min(fromNum, toNum) < 0) return "ERROR";
+    if (fromNum > toNum) [fromNum, toNum] = [toNum, fromNum];
+
     let sum = 0;
     for (let i = fromNum; i <= toNum; i++) {
         sum += i;
