1a8ef5b588335c7b6234f7fbff1d840b7e1bde64
ankhanh-vni <84317220+ankhanh-vni@users.noreply.github.com>
2024-01-02 09:24:45 +0700

    Finished 06 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempInFahrenheit) {
+  // return Math.round((tempInFahrenheit - 32) * 5 / 9 * 10) / 10;
+  let tempInCelsius = (tempInFahrenheit - 32) * 5 / 9;
+  tempInCelsius = parseFloat(tempInCelsius.toFixed(1));
+  return tempInCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempInCelsius) {
+  tempInFahrenheit = tempInCelsius * 9 / 5 + 32;
+  tempInFahrenheit = parseFloat(tempInFahrenheit.toFixed(1));
+  return tempInFahrenheit;
 };
 
 // Do not edit below this line
